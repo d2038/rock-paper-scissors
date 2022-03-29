6f6e12464180f7592272c24fe14b8ba0756046da
d2038 <d2038.2011@gmail.com>
2022-03-29 18:15:13 -0400

    Implement playRound function and helpers

--- script.js
+++ script.js
@@ -11,3 +11,47 @@ function computerPlay() {
       return ""
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = capitalize(playerSelection);
+  const result = getResult(playerSelection, computerSelection);
+  switch (result) {
+    case -1:
+      return `You Lose! ${computerSelection} beats ${playerSelection}`
+    case 0:
+      return `It's a tie! Both threw ${playerSelection}`
+    case 1:
+      return `You Win! ${playerSelection} beats ${computerSelection}`
+    default:
+      return "Something went wrong"
+  }  
+}
+
+function capitalize(string) {
+  return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function getResult(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return 0
+  }
+  switch (playerSelection) {
+    case "Rock":
+      if (computerSelection === "Paper") {
+        return -1
+      }
+      return 1      
+    case "Paper":
+      if (computerSelection === "Rock") {
+        return 1
+      }
+      return -1 
+    case "Scissors":
+      if (computerSelection === "Rock") {
+        return -1
+      }
+      return 1  
+    default:
+      return
+  }
+}
